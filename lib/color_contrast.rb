f03f040fa94509fa30e952f0e4b598ca68998a69
Ronald Chacon <ronald.chacon87@gmail.com>
2018-04-09 01:40:02 -0700

    Update logic
    - add specs
    - bump verison

--- lib/color_contrast.rb
+++ lib/color_contrast.rb
@@ -3,37 +3,58 @@ require "color_contrast/version"
 module ColorContrast
   extend self
 
-  def get_contrast(hex)
-    rgb = get_rgb(hex)
-    yiq = get_yiq(rgb)
+  class InvalidColorError < StandardError; end
 
-    if yiq >= 128
-      "dark"
-    else
-      "light"
+  def self.get_contrast(value)
+    Contrast.new.perform(value)
+  end
+
+  class Contrast
+    def perform(value)
+      raise InvalidColorError, "Not a valid RGB value or HEX value" unless valid_rgb?(value) || valid_hex?(value)
+
+      value = is_rgb?(value) ? value.delete(" ").split(",").map(&:to_i) : get_rgb(value)
+      value = get_yiq(value)
+
+      value >= 128 ? "dark" : "light"
     end
-  end
 
-  private
+    private
 
-  def get_rgb(hex)
-    hex = hex.gsub(/[^0-9A-Z]/i, "")
-    bigint = Integer(hex, 16)
+    def get_rgb(hex)
+      hex = hex.gsub(/[^0-9A-Z]/i, "")
+      hex = hex.length == 3 ? short_to_long_hex(hex) : hex
+      bigint = Integer(hex, 16)
 
-    r = (bigint >> 16) & 255
-    g = (bigint >> 8) & 255
-    b = bigint & 255
+      r = (bigint >> 16) & 255
+      g = (bigint >> 8) & 255
+      b = bigint & 255
 
-    rgb = [r, g, b]
-    rgb
-  end
+      [r, g, b]
+    end
 
-  def get_yiq(rgb)
-    r = rgb[0]
-    g = rgb[1]
-    b = rgb[2]
+    def get_yiq(rgb)
+      r = rgb[0]
+      g = rgb[1]
+      b = rgb[2]
 
-    yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000
-    yiq
+      ((r * 299) + (g * 587) + (b * 114)) / 1000
+    end
+
+    def short_to_long_hex(hex)
+      hex.chars.map { |char| char * 2}.join
+    end
+
+    def valid_hex?(hex)
+      hex && hex.match?(/\A(\h{3}|\h{6})\z/)
+    end
+
+    alias_method :is_hex?, :valid_hex?
+
+    def valid_rgb?(rgb)
+      rgb && rgb.match?(/\A([0-255]{1,3}),\s?([0-255]{1,3}),\s?([0-255]{1,3})\z/)
+    end
+
+    alias_method :is_rgb?, :valid_rgb?
   end
 end
--- spec/color_contrast_spec.rb
+++ spec/color_contrast_spec.rb
@@ -5,7 +5,31 @@ describe ColorContrast do
     expect(ColorContrast::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "RGB value" do
+    context "when value is valid" do
+      it { expect(ColorContrast.get_contrast("255,255,255")).to eq "dark" }
+      it { expect(ColorContrast.get_contrast("0, 0, 0")).to eq "light" }
+    end
+
+    context "when value is invalid" do
+      it { expect { ColorContrast.get_contrast("-255,2555,256") }.to raise_error(ColorContrast::InvalidColorError) }
+    end
+  end
+
+  describe "HEX value" do
+    context "when value is valid" do
+      it { expect(ColorContrast.get_contrast("ffffff")).to eq "dark" }
+      it { expect(ColorContrast.get_contrast("fff")).to eq "dark" }
+      it { expect(ColorContrast.get_contrast("000000")).to eq "light" }
+      it { expect(ColorContrast.get_contrast("000")).to eq "light" }
+    end
+
+    context "when value is invalid" do
+      it { expect { ColorContrast.get_contrast("f") }.to raise_error(ColorContrast::InvalidColorError) }
+
+      it { expect { ColorContrast.get_contrast("ffaa") }.to raise_error(ColorContrast::InvalidColorError) }
+
+      it { expect { ColorContrast.get_contrast("0000000") }.to raise_error(ColorContrast::InvalidColorError) }
+    end
   end
 end
